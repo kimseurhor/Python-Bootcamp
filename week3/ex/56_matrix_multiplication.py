9d843951685992b07857c5fb9cc9cf8ef3931190
kimseurhor <104180526+kimseurhor@users.noreply.github.com>
2022-08-08 23:06:23 +0700

    update

--- week3/ex/56_matrix_multiplication.py
+++ week3/ex/56_matrix_multiplication.py
@@ -1,6 +1,6 @@
 import numpy as np
-def matrix_addition(num1, num2):
-    if num1.shape[1]== num2.shape[0]:
+def matrix_multiplication(num1, num2):
+    if num1.shape== num2.shape:
         res = np.dot(num1, num2)
         return (str(res)).replace('[','').replace(']','')
     else:
@@ -8,5 +8,5 @@ def matrix_addition(num1, num2):
 mat1 = np.array([[3, 7, 5], [2, 6, 7], [4, 3, 2]])
 mat2 = np.array([[6, 5, 4], [3, 2, 1,], [1, 2, 3]])
 print('Martix 1:', '\n', str(mat1).replace('[','').replace(']',''))
-print('Martix 2:', '\n',str(mat2).replace('[','').replace(']',''))
-print('The result matrix is:', '\n' , matrix_addition(mat1, mat2))
+print('Martix 2:', '\n', str(mat2).replace('[','').replace(']',''))
+print('The result matrix is:', '\n' , matrix_multiplication(mat1, mat2))
